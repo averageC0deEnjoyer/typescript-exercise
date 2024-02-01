b0012751f21522208e4a74f695eb799720e87fc6
averageC0deEnjoyer <136006438+averageC0deEnjoyer@users.noreply.github.com>
2024-02-01 16:34:30 +0700

    TS-React-exercise

--- vite-ts/src/model/FullList.ts
+++ vite-ts/src/model/FullList.ts
@@ -11,6 +11,7 @@ interface List {
 
 export default class FullList implements List {
   //singleton
+  // static can be called even though no instance yet
   static instance: FullList = new FullList();
 
   constructor(private _list: ListItem[] = []) {}
@@ -19,30 +20,43 @@ export default class FullList implements List {
     return this._list;
   }
 
-  load() {
-    if (localStorage.getItem('myItems')) {
-      const parsedItemslocalStorage = JSON.parse(
-        localStorage.getItem('myItems')
+  load(): void {
+    //retrieve item from localstorage
+    const storedItems: string | null = localStorage.getItem('myItems');
+    if (typeof storedItems !== 'string') return;
+
+    const parsedItems: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedItems);
+    //TODO console.log(parsed item store item)
+    parsedItems.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
       );
-    }
+      //this using FullList not this._list cause at first we want to initalize instance
+      FullList.instance.addItem(newListItem);
+      // why here cant i just this._list.push(newListItem) // i think can but back to DRY problem
+    });
   }
 
   save(): void {
     localStorage.setItem('myItems', JSON.stringify(this._list));
   }
 
-  clearList() {
+  clearList(): void {
     this._list = [];
-    localStorage.removeItem('myItems');
+    // localStorage.removeItem('myItems');
+    this.save();
   }
 
-  addItem(item: { _id: string; _item: string; _checked: boolean }) {
+  addItem(item: ListItem): void {
     this._list.push(item);
     this.save();
   }
 
-  removeItem(id) {
-    this._list = this._list.filter(item.id === id);
+  removeItem(id: string): void {
+    this._list = this._list.filter((item) => item.id === id);
     this.save();
   }
 }
